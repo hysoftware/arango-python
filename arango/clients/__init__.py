4abd543744d917393958c614023abeffc5adadf9
Abdul Hamid <ah@appscluster.com>
2014-05-22 22:50:39 +0100

    Added basicConfig to logging and separated the use of the libraries available for opening URLs to use either PyCurl or Urllib2 and not to load both.

--- arango/clients/__init__.py
+++ arango/clients/__init__.py
@@ -1,22 +1,29 @@
 import sys
 import logging
-
-from .urllib2client import Urllib2Client
-
+logging.basicConfig()
 
 __all__ = ("Client",)
 
-
 ISPY3 = sys.version_info >= (3, 0)
 
-logger = logging.getLogger("arango.client")
-Client = Urllib2Client
-
+logger = logging.getLogger("arango.clients")
 
 if ISPY3 is False:
     try:
         from .pycurlclient import PyCurlClient
         Client = PyCurlClient
+    except ImportError:
+        try:
+            logger.warning(u"PyCurl not available, trying Urllib2. %s", str(e))
+            from .urllib2client import Urllib2Client
+            Client = Urllib2Client
+        except ImportError:
+            logger.warning(u"Urllib2 not available. %s", str(e))
     except Exception as e:
-        logger.warning(
-            u"Sorry, can't import PyCurlClient. Reason: %s", str(e))
+        logger.warning(u"No library available for opening URLs. %s", str(e))
+else:
+    try:
+        from .urllib2client import Urllib2Client
+        Client = Urllib2Client
+    except ImportError:
+        logger.warning(u"Urllib2 not available. %s", str(e))
